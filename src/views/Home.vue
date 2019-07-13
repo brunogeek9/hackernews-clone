322007cdc5d96512932c28da6fc1747b1dd74338
brunogeek9 <brunogeek9@gmail.com>
2019-07-13 14:50:59 -0300

    mostrando os dados da api na home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <h2>home</h2>
+    <div v-for="story in stories" :key="story">
+      {{ story.title }}
+    </div>
   </div>
 </template>
 
@@ -15,16 +18,38 @@ export default {
   data(){
     return {
       err:'',
-      stories:[
-
-      ]
+      storiesIds:[],
+      stories:[]
     }
+  },
+  computed:{
+    
   },
   async created(){
     // https://hacker-news.firebaseio.com/v0/topstories.json
     try {
-      let request = axios.get('https://hacker-news.firebaseio.com/v0/topstories.json');
-      this.stories = request.data;
+      let response1 = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+      var data = response1.data;
+      this.storiesIds = data.slice(0,11);
+
+      this.storiesIds.forEach(id => {
+        // try {
+        //   let response2 = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
+        //   this.stories.push(response2);
+        // } catch (error) {
+        //   this.$log.error(error);
+        // }
+        axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+        .then((response2)=>{
+          this.stories.push(response2.data)
+        })
+        .catch((err)=>{
+          this.$log.error(err);    
+        });
+            
+          
+      });
+      this.$log.info('test','');
     } catch (error) {
       this.err = error;
     }
