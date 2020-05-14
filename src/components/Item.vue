c9e42596f5fd1ead335a50e3fffd8adf2bd62cad
brunogeek9 <brunogeek9@gmail.com>
2020-05-14 10:35:08 -0300

    listagem dos posts do hackernews com o uso do componente Item

---
+++ src/components/Item.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="story">
+    <span class="score">{{ story.score }}</span>
+    <router-link :to="{ path: '/story/' + story.id }">
+      {{ story.title }}
+      <span>{{ story.url | host }}</span>
+    </router-link>
+    <br />
+    <span class="meta">
+      by {{ story.by }} | {{ story.time }} Ago | {{
+      story.descendants }} comments
+    </span>
+  </div>
+</template>
+<script>
+export default {
+  name: "Item",
+  props: ["story"],
+  components: {
+    // item: Item
+  },
+data: function() {
+    return {
+      err: "",
+      stories: []
+    };
+  },
+//   created(){
+
+//   }
+};
+</script>
+
+<style scoped>
+.story {
+  background-color: #fff;
+  padding: 20px 30px 20px 80px;
+  border-bottom: 1px solid #eee;
+  position: relative;
+  line-height: 20px;
+}
+.score {
+  color: #f60;
+  font-size: 1.1em;
+  font-weight: 700;
+  position: absolute;
+  top: 50%;
+  left: 0;
+  width: 80px;
+  text-align: center;
+  margin-top: -10px;
+}
+.story a {
+  color: #34495e;
+  font-weight: 600;
+  text-decoration: none;
+}
+.story a span {
+  font-size: 0.85em;
+  margin-left: 10px;
+  color: #828282;
+}
+.story .meta {
+  font-size: 0.85em;
+  color: #828282;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,61 +1,63 @@
 <template>
-  <div class="home">
+  <!-- <div class="home">
     <h2>Homepage</h2>
     <div v-for="story in stories" :key="story">
       <h2>{{ story.title }}</h2>
       <p>
-        <router-link :to="{path: `/story/${story.id}`}">
-          {{ story.url }}
-        </router-link>
-        
+        <router-link :to="{path: `/story/${story.id}`}">{{ story.url }}</router-link>
       </p>
       <p>Coments: {{ story.descendants }}</p>
       <p>Score: {{story.score}}</p>
     </div>
+  </div> -->
+  <div>
+    <h2>Homepage</h2>
+    <item v-for="story in stories" :key="story.id" :story="story"></item>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
+import Item from "../components/Item";
 
 export default {
-  name: 'home',
-  data(){
+  name: "home",
+  components: {
+    'item': Item
+  },
+  data() {
     return {
-      err:'',
-      storiesIds:[],
-      stories:[]
-    }
+      err: "",
+      storiesIds: [],
+      stories: []
+    };
   },
-  computed:{
-    
-  },
-  methods:{
-    
-  },
-  async created(){
+  computed: {},
+  methods: {},
+  async created() {
     try {
       // getting all ids
-      let response1 = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+      let response1 = await axios.get(
+        "https://hacker-news.firebaseio.com/v0/topstories.json"
+      );
       var data = response1.data;
-      this.storiesIds = data.slice(0,11);
+      this.storiesIds = data.slice(0, 11);
       // getting all news
       this.storiesIds.forEach(id => {
-        axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
-        .then((response2)=>{
-          this.stories.push(response2.data)
-        })
-        .catch((err)=>{
-          this.$log.error(err);    
-        });    
+        axios
+          .get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+          .then(response2 => {
+            this.stories.push(response2.data);
+          })
+          .catch(err => {
+            this.$log.error(err);
+          });
       });
 
-      this.$log.info('test','');
+      this.$log.info("test", "");
     } catch (error) {
       this.err = error;
     }
-    
-
   }
-}
+};
 </script>
