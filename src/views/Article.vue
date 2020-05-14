3648c856eddd23f298fcfd8884b02bcdbd4d0a29
brunogeek9 <brunogeek9@gmail.com>
2020-05-14 09:14:42 -0300

    adição de estilização global a aplicação

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,52 +1,121 @@
 <template>
-    <div class="container">
-        <h2>{{story.title}}</h2>
-        <p>Score: {{story.score}}</p>
-        <p>
-            original source 
-            <a :href="story.url">Link: {{story.url}}</a>
-        </p>
-        <br>
-        <hr>
-        <div class="coments" v-for="coment in coments" :key="coment.id">
-            <div class="coment-wrap">
-                <p>{{ coment.text }}</p>
-                <p>{{ coment.by }}</p>
-                <p>{{ coment.time }}</p>                
-            </div>
-            <hr>
-        </div>
+  <div class="container">
+    <h2>{{story.title}}</h2>
+    <p>Score: {{story.score}}</p>
+    <p>
+      original source
+      <a :href="story.url" target="_blank">Link: {{story.url}}</a>
+    </p>
+    <br />
+    <hr />
+    <div class="coments" v-for="coment in coments" :key="coment.id">
+      <div class="coment-wrap">
+        <p>{{ coment.text }}</p>
+        <p>{{ coment.by }}</p>
+        <p>{{ coment.time }}</p>
+      </div>
+      <hr />
     </div>
+  </div>
 </template>
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-    name: 'article',
-    data(){
-        return{
-            story: {},
-            err: '',
-            coments: []
-        }
-    },
-    async created(){
-        
-        try {
-            let res1 = await axios.get(`https://cors-anywhere.herokuapp.com/https://hacker-news.firebaseio.com/v0/item/${this.$route.params.id}.json`);
-            this.story = res1.data;
-            this.story.coments = [];   
-            this.story.kids.forEach(id => {
-                axios.get(`https://cors-anywhere.herokuapp.com/https://hacker-news.firebaseio.com/v0/item/${id}.json`)
-                .then((res2)=>{
-                    this.coments.push(res2.data);
-                })
-            }); 
-        } catch (error) {
-            this.err = error;
-        }
+  name: "article",
+  data() {
+    return {
+      story: {},
+      err: "",
+      coments: []
+    };
+  },
+  async created() {
+    try {
+      let res1 = await axios.get(
+        `https://cors-anywhere.herokuapp.com/https://hacker-news.firebaseio.com/v0/item/${this.$route.params.id}.json`
+      );
+      this.story = res1.data;
+      this.story.coments = [];
+      this.story.kids.forEach(id => {
+        axios
+          .get(
+            `https://cors-anywhere.herokuapp.com/https://hacker-news.firebaseio.com/v0/item/${id}.json`
+          )
+          .then(res2 => {
+            this.coments.push(res2.data);
+          });
+      });
+    } catch (error) {
+      this.err = error;
     }
-}
+  }
+};
 </script>
-<style>
 
+<style scoped>
+.comment-wrap {
+  margin-bottom: 1.25rem;
+  display: table;
+  width: 100%;
+  min-height: 5.3125rem;
+}
+
+.photo {
+  padding-top: 0.625rem;
+  display: table-cell;
+  width: 3.5rem;
+}
+.photo .avatar {
+  height: 2.25rem;
+  width: 2.25rem;
+  border-radius: 50%;
+  background-size: contain;
+}
+
+.comment-block {
+  padding: 1rem;
+  background-color: #fff;
+  display: table-cell;
+  vertical-align: top;
+  border-radius: 0.1875rem;
+  -webkit-box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.08);
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.08);
+}
+.comment-block textarea {
+  width: 100%;
+  resize: none;
+}
+
+.comment-text {
+  margin-bottom: 1.25rem;
+}
+
+.bottom-comment {
+  color: #acb4c2;
+  font-size: 0.875rem;
+}
+
+.comment-date {
+  float: left;
+}
+
+.comment-actions {
+  float: right;
+}
+.comment-actions li {
+  display: inline;
+  margin: -2px;
+  cursor: pointer;
+}
+.comment-actions li.complain {
+  padding-right: 0.75rem;
+  border-right: 1px solid #e1e5eb;
+}
+.comment-actions li.reply {
+  padding-left: 0.75rem;
+  padding-right: 0.125rem;
+}
+.comment-actions li:hover {
+  color: #0095ff;
+}
 </style>
